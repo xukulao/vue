3a17ed6aaaa8b4d9e88820bdc9e9269508307677
keyongteacher <keke@qq.com>
2021-02-27 21:44:31 +0800

    es6 module import/export/default/as/obj and vue2 set/delete/watch

---
+++ m4.js
@@ -0,0 +1,7 @@
+export let name="jack";
+export function changeName(v) {
+    name=v;
+};
+
+let nums=[1,2,3,5];
+export {nums};
---
+++ m5.js
@@ -0,0 +1,10 @@
+//export default username="lucy";
+//var usename="jack";
+//export default username;
+//export default {username};
+export default {
+    username:"jack",
+    info:function () {
+        console.log(this.username);
+    }
+}
